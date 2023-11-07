ec921461e2c7580eec882b9c7ae41c533ef3016b
Umang Sehgal <49648516+Umang-Sehgal@users.noreply.github.com>
2023-11-07 15:49:44 +0530

    Create script.py

---
+++ .github/workflows/script.py
@@ -0,0 +1,6 @@
+import requests
+response = requests.post(
+  "https://huggingface.co/api/repos/create",
+  json={"private":"True","name":"github","organization":"shellplc","type":"model"},
+  headers={"Authorization":"Bearer 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"}
+)
