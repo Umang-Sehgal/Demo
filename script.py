844ca903f6910f4651b76930833a3ed866a58831
Umang Sehgal <49648516+Umang-Sehgal@users.noreply.github.com>
2023-11-07 19:38:37 +0530

    Update script.py

--- script.py
+++ script.py
@@ -4,6 +4,5 @@ response = requests.post(
   json={"private":"True","name":"github","organization":"shellplc","type":"model"},
   headers={"Authorization":"Bearer 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"}
 )
-if(response.status_code==409){
+if(response.status_code==409):
   !git push https://Umang-Sehgal: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@huggingface.co/shellplc/github main
-}
