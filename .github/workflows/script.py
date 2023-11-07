ff52dc2f8a9416d74e4a894bedc627dce304c877
Umang Sehgal <49648516+Umang-Sehgal@users.noreply.github.com>
2023-11-07 15:56:32 +0530

    Delete .github/workflows/script.py

--- .github/workflows/script.py
+++
@@ -1,6 +0,0 @@
-import requests
-response = requests.post(
-  "https://huggingface.co/api/repos/create",
-  json={"private":"True","name":"github","organization":"shellplc","type":"model"},
-  headers={"Authorization":"Bearer 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"}
-)
