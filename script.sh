0aebfd2224e168efe857d3525a94bc73ba623577
Umang Sehgal <49648516+Umang-Sehgal@users.noreply.github.com>
2023-11-09 12:28:20 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-for n in */;
+for dir in */;
 do
-   echo $n
+          curl --location 'https://huggingface.co/api/repos/create' \
+          --header 'Authorization: Bearer ${{ env.HF_TOKEN }}' \
+          --header 'Content-Type: application/json' \
+          --data '{
+            "type": "model",
+            "name": "$dir",
+            "organization": "shellplc",
+            "private":true
+          }'
+          git push --force https://$HF_USERNAME:$HF_TOKEN@huggingface.co/shellplc/$dir main/$dir 
+#    echo $dir
 done
